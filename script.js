fb38509f92caec694f50492b6fc0e216352175e9
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-19 18:16:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
+	event.preventDefault();
+	  const radiusInput = document.getElementById('radius').value;
+	 const radius = parseFloat(radiusInput);
+	const volumeOutput = document.getElementById('volume');
+	  if (isNaN(radius) || radius < 0) {
+        volumeOutput.value = 'NaN';
+        return;
+    }
+	   const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+	   volumeOutput.value = volume.toFixed(4);
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
